5c540e24b763463e46d7a8b393e546eddc4e5d2a
NoNormalCreeper <2560359315@qq.com>
2022-09-08 18:19:45 +0800

    🚧 bg, sprite, cursor

--- js/main.js
+++ js/main.js
@@ -1,24 +1,69 @@
 var config = {
     type: Phaser.AUTO,
-    width: 720,
-    height: 480,
+    width: 960,
+    height: 600,
     scene: {
         preload: preload,
         create: create,
         update: update
-    }
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false
+        }
+    },
 };
 
 var game = new Phaser.Game(config);
 
 function preload() {
-    this.load.image('background', '../assets/bg.png');
+    assetDict = {
+        // 'name': 'file_name'
+        'background': 'bg',
+        'background2': 'bg2',
+        'chisatoStop': 'stop',
+        'chisatoLeft': 'left',
+        'chisatoRight': 'right',
+        'shotFlash': 'shot_flash',
+        'shotHole': 'hole',
+        'aimCursor': 'aim',
+    }
+    for (key in assetDict) {
+        this.load.image(key, `../assets/${assetDict[key]}.png`);
+    }
+    this.load.spritesheet('chisato', 
+        '../assets/chisato-sprite.png',
+        { frameWidth: 960/3 }
+    );
 }
 
 function create() {
-    this.add.image(0, 0, 'background').setOrigin(0, 0);
+    this.add.image(480, 300, 'background');
+    var chisato = this.physics.add.sprite(480, 450, 'chisato');
+    chisato.setCollideWorldBounds(true);
+    this.anims.create({
+        key: 'left',
+        frames: [ { key: 'chisato', frame: 0 } ],
+        frameRate: 10
+    });
+    this.anims.create({
+        key: 'right',
+        frames: [ { key: 'chisato', frame: 2 } ],
+        frameRate: 10
+    });
+    this.anims.create({
+        key: 'center',
+        frames: [ { key: 'chisato', frame: 1 } ],
+        frameRate: 10
+    });
+    var aim = this.add.image(480, 300, 'aimCursor');
+    this.input.on('pointermove', function (pointer) {
+        aim.x = pointer.x;
+        aim.y = pointer.y;
+    });
 }
 
 function update() {
-    ;
+    
 }
