e44f63d1137f6106a71f56598039450fde0a3163
NoNormalCreeper <2560359315@qq.com>
2022-09-09 11:00:37 +0800

    🐛 add aim center to improve accuracy

--- js/main.js
+++ js/main.js
@@ -28,42 +28,54 @@ function preload() {
         'shotFlash': 'shot_flash',
         'shotHole': 'hole',
         'aimCursor': 'aim',
+        'aimCenter': 'aim_center'
     }
     for (key in assetDict) {
         this.load.image(key, `../assets/${assetDict[key]}.png`);
     }
     this.load.spritesheet('chisato', 
         '../assets/chisato-sprite.png',
-        { frameWidth: 960/3 }
+        { frameWidth: 960/3, frameHeight: 400 }
     );
 }
 
 function create() {
     this.add.image(480, 300, 'background');
-    var chisato = this.physics.add.sprite(480, 450, 'chisato');
+    chisato = this.physics.add.sprite(480, (600-400/2), 'chisato');
     chisato.setCollideWorldBounds(true);
     this.anims.create({
         key: 'left',
         frames: [ { key: 'chisato', frame: 0 } ],
-        frameRate: 10
+        frameRate: 1
     });
     this.anims.create({
         key: 'right',
         frames: [ { key: 'chisato', frame: 2 } ],
-        frameRate: 10
+        frameRate: 1
     });
     this.anims.create({
         key: 'center',
         frames: [ { key: 'chisato', frame: 1 } ],
-        frameRate: 10
+        frameRate: 1
     });
-    var aim = this.add.image(480, 300, 'aimCursor');
+    chisato.anims.play('center')
+
+    aim = this.physics.add.image(480, 300, 'aimCursor');
+    aimCenter = this.physics.add.image(480, 300, 'aimCenter')
     this.input.on('pointermove', function (pointer) {
         aim.x = pointer.x;
         aim.y = pointer.y;
     });
+    // this.input.on('pointerdown', () => {
+    //     chisato.anims.play('left');
+    // });
+
+    this.physics.add.overlap(aimCenter, chisato, (aim, chisato) => {
+        console.log(1);
+    });
 }
 
 function update() {
-    
+    aimCenter.x = aim.x
+    aimCenter.y = aim.y
 }
